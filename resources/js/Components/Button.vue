62fbbe8d4566b6baa6124036b40f5b7bc1b9732b
KaykySousa <kaykysousa321@gmail.com>
2022-08-29 17:01:34 -0300

    initial project

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="bg-cyan-500 text-white h-12 px-4 rounded-3xl">
+        <slot />
+    </button>
+</template>
---
+++ resources/js/Pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Button from "../Components/Button.vue";
+
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+<template>
+    <div
+        class="min-h-screen w-full bg-neutral-50 flex flex-col justify-center items-center"
+    >
+        <h1 class="text-2xl mb-4">Initial Project With Laravel and Vue</h1>
+        <Button @click="count++"
+            >The button was clicked {{ count }} times</Button
+        >
+    </div>
+</template>
