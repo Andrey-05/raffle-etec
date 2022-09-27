3aff48641a680f33e5ade893a24b8788bf39aa6f
KaykySousa <kaykysousa321@gmail.com>
2022-09-26 22:46:34 -0300

    add: input component

---
+++ resources/js/Components/Input.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    labeltext: String,
+});
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <label class="uppercase text-sky-600 font-bold">{{ labeltext }}</label>
+        <input
+            v-bind="$attrs"
+            class="w-full h-10 bg-transparent outline-0 border-b-2 border-sky-600"
+        />
+    </div>
+</template>
