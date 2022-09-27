c66fc3d35b2fb89a856f7f2e9bd5ee7e4965fe56
KaykySousa <kaykysousa321@gmail.com>
2022-09-26 22:54:10 -0300

    add: button component

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,5 +1,7 @@
 <template>
-    <button class="bg-cyan-500 text-white h-12 px-4 rounded-3xl">
+    <button
+        class="w-full h-12 text-white bg-cyan-600 rounded-md uppercase font-bold hover:bg-cyan-700"
+    >
         <slot />
     </button>
 </template>
