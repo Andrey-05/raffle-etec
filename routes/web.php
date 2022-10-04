692d47ba4e2b209fc02c7c9696917e6fee4e4f49
KaykySousa <kaykysousa321@gmail.com>
2022-10-03 22:45:03 -0300

    add: post /user route to create users

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -17,3 +18,5 @@ use Inertia\Inertia;
 Route::get('/register', function () {
     return Inertia::render("register");
 });
+
+Route::post('/user', [UserController::class, "create"]);
