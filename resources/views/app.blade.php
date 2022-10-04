1f87b5bf27c57f94cdb6083dae63342e99754bcd
KaykySousa <kaykysousa321@gmail.com>
2022-10-03 22:44:14 -0300

    add: csrf token to meta tag in head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
     @inertiaHead
 </head>
+
 <body>
     @inertia
 </body>
+
 </html>
