6b972ba6bd19e38fd2077de11b1f596a1abafeb8
KaykySousa <kaykysousa321@gmail.com>
2022-10-03 22:42:57 -0300

    add: csrf token vue component

---
+++ resources/js/Components/CSRF.vue
@@ -0,0 +1,7 @@
+<template>
+    <input type="hidden" name="_token" :value="csrf" />
+</template>
+
+<script setup>
+const csrf = document.head.querySelector("meta[name='csrf-token']").content;
+</script>
