237ff741965eac6b62f8fa2c0bf6ca9f51977b2c
andrey <andreylrodrigues@hotmail.com>
2022-11-27 20:39:07 -0300

    update: button can recive style params

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,6 +1,25 @@
+<script setup>
+    defineProps({
+        hasBackground: Boolean,
+        color: String
+    })
+</script>
+
 <template>
     <button
-        class="w-full h-12 text-white bg-cyan-600 rounded-md uppercase font-bold hover:bg-cyan-700"
+        :class="
+            `w-full h-12 rounded-md uppercase font-bold
+            ${hasBackground
+                ? color == 'sky' 
+                    ? 'bg-sky-600 text-white hover:bg-sky-700' 
+                    : color == 'red' 
+                    && 'bg-red-600 text-white hover:bg-red-700'
+                : color == 'sky'
+                    ? 'text-sky-600 hover:text-sky-700' 
+                    : color == 'red' 
+                    && 'text-red-600 hover:text-red-700'
+            }`
+        "
     >
         <slot />
     </button>
