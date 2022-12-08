74f605cb988affdc005a9323afdbb44191a88e29
andrey <andreylrodrigues@hotmail.com>
2022-12-08 14:56:58 -0300

    update: add size and new colors to button

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,24 +1,39 @@
 <script setup>
     defineProps({
         hasBackground: Boolean,
-        color: String
+        color: String,
+        size: String
     })
 </script>
 
 <template>
     <button
         :class="
-            `w-full h-12 rounded-md uppercase font-bold
+            `h-12 rounded-md uppercase font-bold
             ${hasBackground
                 ? color == 'sky' 
                     ? 'bg-sky-600 text-white hover:bg-sky-700' 
                     : color == 'red' 
-                    && 'bg-red-600 text-white hover:bg-red-700'
+                    ? 'bg-red-600 text-white hover:bg-red-700'
+                    : color == 'lightSky'
+                    && 'bg-sky-200 text-sky-600 hover:bg-sky-300'
                 : color == 'sky'
                     ? 'text-sky-600 hover:text-sky-700' 
                     : color == 'red' 
-                    && 'text-red-600 hover:text-red-700'
-            }`
+                    ? 'text-red-600 hover:text-red-700'
+                    : color == 'lightSky'
+                    && 'text-sky-300 hover:text-sky-400'
+            }
+            ${
+                size == 'full'
+                ? 'w-full'
+                : size == '1/2'
+                ? 'w-1/2'
+                : size == 'auto'
+                ? 'w-auto'
+                : 'w-full'
+            }
+            `
         "
     >
         <slot />
